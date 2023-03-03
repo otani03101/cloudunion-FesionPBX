0e2a66aceefd231b055119d2127083a004fb7810
otani03101 <100661837+otani03101@users.noreply.github.com>
2023-03-03 14:18:58 +0900

    情報取得（同じドメインの存在確認）

--- domains.php
+++ domains.php
@@ -1,13 +1,24 @@
 <?php
+$domain_name = $_GET['domain'];
+error_log($log . $domain_name);
+
 try {
-    $sql = 'SELECT * FROM v_domains';
+    $sql = 'SELECT domain_name FROM v_domains WHERE domain_name = :domain_name';
     $stmh = $pdo->prepare($sql);
+    $stmh->bindValue(':domain_name', $domain_name, PDO::PARAM_STR);
     $stmh->execute();
+    $count = $stmh->rowCount();
+    error_log($log . $count);
 } catch (Exception $exception) {
     error_log($log . 'エラー:' . $exception->getMessage());
 }
 
-$row = $stmh->fetch(PDO::FETCH_ASSOC);
-
-echo $row['domain_name'];
-error_log($log . $row['domain_name']);
+if ($count > 0) {
+    $value_array = ['DomainName' => true];
+    $jsonstr = json_encode($value_array);
+    echo $jsonstr;
+} else {
+    $value_array = ['DomainName' => false];
+    $jsonstr = json_encode($value_array);
+    echo $jsonstr;
+}
